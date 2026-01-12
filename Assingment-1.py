14cff08a27c06ca115c4663944755c91d531df32
Himanshu shekhar <shekhar55003@gmail.com>
2026-01-12 08:06:26 +0530

    Assingment-1 coding

---
+++ Assingment-1.py
@@ -0,0 +1,36 @@
+a = 10
+b = 5
+print(a**b%3)
+
+
+j=6
+g=3.3
+print(j/g)
+print(type(j/g))
+print(j//g)
+print(type(j//g))
+
+a = "10"
+b=float(a)+5
+result=str(b)+"123"
+print(type(result))
+
+a = 15
+b = 3
+c = 4
+result=a+b*c//(c%b)-5
+print(result)
+
+a=4
+b=5
+a*=b*2
+print(a)
+
+a=3
+b=5
+c=(a==3) and (b==5) or (a!=3)
+print(c)
+
+a=5
+b=3
+print(a&b)
